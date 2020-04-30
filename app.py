b927a61cf578e9f3055f6c8fd113f1796f05c519
jakobowsky <jakob.wolitzki@gmail.com>
2020-05-01 01:32:25 +0200

    Finished first part

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,55 +6,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
-
 @app.route('/')
 def index():
-    incomplete = Todo.query.filter_by(complete=False).all()
-    complete = Todo.query.filter_by(complete=True).all()
-
-    return render_template('index.html', incomplete=incomplete, complete=complete)
-
-
-@app.route('/add', methods=['POST'])
-def add():
-    todo = Todo(text=request.form['todoitem'], complete=False)
-    db.session.add(todo)
-    db.session.commit()
-
-    return redirect(url_for('index'))
-
-
-@app.route('/complete/<id>')
-def complete(id):
-    todo = Todo.query.filter_by(id=int(id)).first()
-    todo.complete = True
-    db.session.commit()
-
-    return redirect(url_for('index'))
-
-
-@app.route('/uncheck/<id>')
-def uncheck(id):
-    todo = Todo.query.filter_by(id=int(id)).first()
-    todo.complete = False
-    db.session.commit()
-
-    return redirect(url_for('index'))
-
-
-@app.route('/delete/<id>')
-def delete(id):
-    todo = Todo.query.filter_by(id=int(id)).first()
-    db.session.delete(todo)
-    db.session.commit()
-
-    return redirect(url_for('index'))
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
